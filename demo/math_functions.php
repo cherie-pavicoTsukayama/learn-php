8b901fa6d0faccac730c8e3e9e54a5a9059cb5d8
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2020-08-01 12:52:11 -0700

    created examples of the common math functions

---
+++ demo/math_functions.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    echo 'get the power of 2^7 using the php function pow(2, 7): ' . pow(2, 7) . "<br>";
+    echo 'get a random number between 1 - 1000 using the php function rand(1, 1000): ' . rand(1, 1000) . "<br>";
+    echo 'get a random number between 1 - 1000 using the php function rand(1, 1000): ' . rand(1, 1000) . "<br>";
+    echo 'get a random number between 1 - 1000 using the php function rand(1, 1000): ' . rand(1, 1000) . "<br>";
+    echo 'get the square root of 100 using the php function sqrt(100): ' . sqrt(100) . "<br>";
+    echo 'round 4.6 up using the php function ceil(4.6): ' . ceil(4.6) . "<br>";
+    echo 'round 4.6 down using the php function floor(4.6): ' . floor(4.6) . "<br>";
+    echo 'round 4.6 using the php function round(4.6): ' . round(4.6) . "<br>";
+    echo 'round 4.6 using the php function round(4.3): ' . round(4.3) . "<br>";
+
+
+
+
+
+
+  ?>
+</body>
+</html>
