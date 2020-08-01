5df6cb2369e1a174bc04b62039b50ae9505d4e05
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2020-08-01 11:34:14 -0700

    learning about the arythmatic in php

---
+++ demo/math.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+  <?php
+    echo "addition 56 + 45";
+    echo "<br>";
+    echo 56 + 45;
+    echo "<br>";
+    echo "subtraction 56 - 45";
+    echo "<br>";
+    echo 56 - 45;
+    echo "<br>";
+    echo "multiplication 56 * 45";
+    echo "<br>";
+    echo 56 * 45;
+    echo "<br>";
+    echo "division 56 / 45";
+    echo "<br>";
+    echo 56 / 45;
+  ?>
+
+</body>
+</html>
