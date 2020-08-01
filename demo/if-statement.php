0b734806c3f130a1f7a13c0b9a525730e0ac75fa
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2020-08-01 12:09:53 -0700

    if statements work the same as JS they just need to be put inside of the php tags

---
+++ demo/if-statement.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+
+    if(1 < 10){
+      echo "1 is less than 10";
+    };
+
+  ?>
+</body>
+</html>
