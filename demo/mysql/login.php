f8c26537f5b42856ec30c74c6710e016b6c1435e
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2020-08-02 11:42:24 -0700

    created a variable that takes the api mysqli_connect and passed in 4 parameters that tell php what the server is, whats the username, what the password is and what the database name is.

--- demo/mysql/login.php
+++ demo/mysql/login.php
@@ -3,12 +3,30 @@ if(isset($_POST['submit'])) {
   $username = $_POST['username'];
   $password = $_POST['password'];
 
-  if($username && $password) {
-    echo "Your username: " . $username . "<br>";
-    echo "Your password: ".$password;
+  //Create a variable to run the function mysqli_connect <=[this is an API] to connect to the database
+  //mysqli_connect is an API
+  //first parameter is the server ex: localhost
+  //second parameter is the username ex: root
+  //third parameter is the password
+  //forth parameter is the database name ex: loginapp
+  $connection  = mysqli_connect('localhost', 'root', '', 'loginapp');
+  if($connection) {
+    echo "connected to the database";
   } else {
-    echo "Username or password is not filled out";
+    //will not excute any code and will report a specified error
+    die("Database connection failed");
   }
+
+
+
+
+
+  // if($username && $password) {
+  //   echo "Your username: " . $username . "<br>";
+  //   echo "Your password: ".$password;
+  // } else {
+  //   echo "Username or password is not filled out";
+  // }
 
 
 }
