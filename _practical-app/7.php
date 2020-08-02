d09abebfb9f433da6f792ebd21309d1f36b24f67
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2020-08-02 16:08:49 -0700

    added the form, created a database, set the db.php to connect to the db

--- _practical-app/7.php
+++ _practical-app/7.php
@@ -1,22 +1,23 @@
 <?php include "functions.php"; ?>
+<?php include "db.php"; ?>
 <?php include "includes/header.php";?>
-    
+
 
 	<section class="content">
 
 		<aside class="col-xs-4">
 
 		<?php Navigation();?>
-			
-			
+
+
 		</aside><!--SIDEBAR-->
 
 
 	<article class="main-content col-xs-8">
-	
-	
-	
-	<?php  
+
+
+
+	<?php
 
 	/*  Step 1 - Create a database in PHPmyadmin
 
@@ -27,9 +28,24 @@
 		Step 4 - Connect to Database and read data
 
 */
-	
-	?>
+	if(isset($_POST['name'])) {
 
+	}
+	?>
+	<div>
+		<h1>Dogs</h1>
+		<form action="7.php" method="post">
+			<div class="form-group">
+				<label for="name">Name</label>
+				<input class="form-control" type="text" name="name" >
+			</div>
+			<div class="form-group">
+				<label for="age">Age</label>
+				<input class="form-control" type="text" name="age" >
+			</div>
+			<input class="btn btn-primary" type="submit" name="submit" value="submit">
+		</form>
+	</div>
 
 
 
