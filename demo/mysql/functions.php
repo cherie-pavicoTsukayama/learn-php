a05a4a8ed7b6d12da0141ebccdd2ff409e0000d8
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2020-08-02 15:53:19 -0700

    refactored the header and the fotter out of the php files.

--- demo/mysql/functions.php
+++ demo/mysql/functions.php
@@ -25,6 +25,21 @@
 }
   }
 
+  function readRows() {
+    global $connection;
+    $query = "SELECT * FROM users";
+    $result = mysqli_query($connection, $query);
+
+    //the below code checks if the result is false then runs the die method which
+    //cancels out of the code and returns and error.
+    if(!$result) {
+      die('Query FAILED' . mysqli_error($connection));
+    }
+    while($row = mysqli_fetch_assoc($result)) {
+      print_r($row);
+    }
+  }
+
   function showAllData() {
     global $connection;
     $query = "SELECT * FROM users";
--- demo/mysql/login_delete.php
+++ demo/mysql/login_delete.php
@@ -2,46 +2,31 @@
   include "db.php";
   include "functions.php";
 ?>
-<?php
+<?php deleteRows();?>
 
-  deleteRows();
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <title>Document</title>
-</head>
-<body>
-
-  <div class="container">
-        <div class="col-sm-8 mt-5">
-          <h1 class="text-center">Delete A Row</h1>
-          <form action="login_delete.php" method="post">
-            <div class="form-group">
-              <label for="username">Username</label>
-              <input class="form-control" type="text" name="username" placeholder="Username">
-            </div>
-            <div class="form-group">
-              <label for="password">Password</label>
-              <input class="form-control" type="password" name="password" placeholder="Password">
-            </div>
-            <div class="form-group">
-              <select name="id" id="">
-                <?php
-                  showAllData();
-                ?>
-
-              </select>
-            </div>
-            <input class="btn btn-danger" type="submit" name="submit" value="Delete">
-          </form>
-        </div>
+<?php include 'includes/header.php' ?>
+<div class="container">
+  <div class="col-sm-8 mt-5">
+    <h1>Delete A Row</h1>
+    <form action="login_delete.php" method="post">
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input class="form-control" type="text" name="username" placeholder="Username">
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input class="form-control" type="password" name="password" placeholder="Password">
+      </div>
+      <div class="form-group">
+        <select name="id" id="">
+          <?php
+            showAllData();
+          ?>
+        </select>
+      </div>
+      <input class="btn btn-danger" type="submit" name="submit" value="Delete">
+    </form>
   </div>
+</div>
 
-</body>
-</html>
+<?php include 'includes/footer.php' ?>
