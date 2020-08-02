a1dbb2593ea31291d215e98e3f421207002910ec
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2020-08-01 17:07:01 -0700

    username validation and checking to see if the users name is in the name array

--- demo/checking-for-form-submission.php
+++ demo/checking-for-form-submission.php
@@ -1,5 +1,6 @@
 <?php
 if(isset($_POST['submit'])) {
+  $name = ["Edwin", "Cherie", "Parker", "Shane", "Irene"];
   $username = $_POST['username'];
   $password = $_POST['password'];
   if (strlen($username) === 0) {
@@ -7,6 +8,11 @@ if(isset($_POST['submit'])) {
   } else {
     echo 'Your username is: ' . $username;
   }
+  if(in_array($username, $name)){
+    echo "<br>";
+    echo "Your username has been verified";
+  }
+
 }
 
 ?>
