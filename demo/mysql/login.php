b7534e1402f74b35f3747e41179efd648b00d66c
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2020-08-02 11:33:28 -0700

    added requried to the input tags

--- demo/mysql/login.php
+++ demo/mysql/login.php
@@ -33,11 +33,11 @@ if(isset($_POST['submit'])) {
       <form action="login.php" method="post">
         <div class="form-group">
           <label for="username">Username</label>
-          <input type="text" name="username" class="form-control">
+          <input required type="text" name="username" class="form-control">
         </div>
         <div class="form-group">
           <label for="password">Password</label>
-          <input type="password" name="password" class="form-control">
+          <input required type="password" name="password" class="form-control">
         </div>
         <input class="btn btn-primary" type="submit" name="submit" value="Submit">
       </form>
