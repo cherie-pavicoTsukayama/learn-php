c6caf3f3db9eb8b16994eb4ea5230c07a5fab2ea
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2020-08-04 15:46:26 -0700

    added the 3 variables to the setcookie method, checked the cookie and saw that it is set

--- demo/cookies.php
+++ demo/cookies.php
@@ -6,10 +6,11 @@
   $value = 100;
   //the time() method is in seconds. It takes returns the current time in seconds.
   // Below we are setting the expiration to the current time PLUS (60 seconds * 60 Min * 24 hours * 7)
-  // the above equation equals out to a week in seconds. 
+  // the above equation equals out to a week in seconds.
   $expiration = time() + (60 * 60 * 24 * 7);
-  setcookie()
+  setcookie($name, $value, $expiration);
 
+  
 ?>
 
 
