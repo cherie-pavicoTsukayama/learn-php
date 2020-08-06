0ded8eec546ba6baa7fae9992578313c83cc3d11
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2020-08-06 16:04:41 -0700

    updated a typo

--- cms/index.php
+++ cms/index.php
@@ -50,7 +50,7 @@
              </div>
 
             <!-- Blog Sidebar Widgets Column -->
-            <?php include 'includes/sidebar.php' ?>\
+            <?php include 'includes/sidebar.php' ?>
 
         </div>
 
