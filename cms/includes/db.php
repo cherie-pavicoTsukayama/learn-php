4cd31e0fb1a915e33510cfd274143107f616fc26
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2020-08-06 14:46:15 -0700

    Refactored the code to create an array of the arguments that are needed for the mysqli_connect method. Using the foreach method to go through the array and execute a command that turns each key into a constatnt.

--- cms/includes/db.php
+++ cms/includes/db.php
@@ -1,5 +1,19 @@
 <?php
-  $connection = mysqli_connect('localhost', 'root', '', 'cms');
+
+  $db['db_host'] = "localhost";
+  $db['db_user'] = "root";
+  $db['db_pass'] = "";
+  $db['db_name'] = "cms";
+
+  //convert array into constatns by using the foreach method.
+  //first argument is the array
+  //second argument is the key ie. db_host db_user db_pass
+
+  foreach($db as $key => $value) {
+    define(strtoupper($key), $value);
+  }
+//use the constants as arguemnts in the mysqli_connect method. 
+  $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
   if($connection) {
     echo 'You are connected';
   }
