c1ff7571ff76cda915efbd965d709c965ba3d1eb
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2020-08-09 10:19:01 -0700

    updated the categoreis and the amin index php files to include the renamed admin_navigation.php

--- cms/admin/categories.php
+++ cms/admin/categories.php
@@ -3,7 +3,7 @@
     <div id="wrapper">
 
         <!-- Navigation -->
-       <?php include 'includes/navigation.php' ?>
+       <?php include 'includes/admin_navigation.php' ?>
         <div id="page-wrapper">
 
         <div class="container-fluid">
