f1ac2e3bffd8d1fdaac3aa7174df61a41d63cebf
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2020-08-09 13:39:19 -0700

    created a function.php to hold the functions that are in the categories.php.

--- cms/admin/categories.php
+++ cms/admin/categories.php
@@ -16,22 +16,7 @@
                         <small>Author</small>
                     </h1>
                     <div class="col-sm-6">
-                        <?php
-                        if(isset($_POST['submit'])) {
-                          $cat_title = $_POST['cat_title'];
-                          $cat_title = mysqli_real_escape_string($connection, $cat_title);
-                          if($cat_title === '' || empty($cat_title)) {
-                            echo 'This field should not be empty.';
-                          } else {
-                            $query = "INSERT INTO categories(cat_title) ";
-                            $query .= "VALUES('{$cat_title}') ";
-                            $create_category_query = mysqli_query($connection, $query);
-                            if(!$create_category_query) {
-                              die('QUERY FAILED' . mysqli_error($connection));
-                            }
-                          }
-                        }
-                        ?>
+                        <?php insertCategories(); ?>
 <!-- Add Category -->
                       <form action="categories.php" method="post">
                         <div class="form-group">
