494c9d89a2293b78e6401cdcb954253fb2ffe27b
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2020-08-09 13:39:49 -0700

    added the find all categories php to the functions.php and the delete category to the functions.php

--- cms/admin/categories.php
+++ cms/admin/categories.php
@@ -53,38 +53,10 @@
                           </tr>
                         </thead>
                         <tbody>
-<?php //FIND ALL CATEGORIES QUERY
-$query = "SELECT * FROM categories ";
-$select_categories = mysqli_query($connection, $query);
-
-  while($row = mysqli_fetch_assoc($select_categories)) {
-    $cat_title = $row['cat_title'];
-    $cat_id = $row['cat_id'];
-    echo "<tr>";
-    echo "<td>{$cat_id}</td>";
-    echo "<td>{$cat_title}</td>";
-    echo "<td>
-            <a href='categories.php?delete={$cat_id}'>
-              Delete
-            </a>
-          </td>";
-    echo "<td>
-            <a href='categories.php?edit={$cat_id}'>
-              Edit
-            </a>
-          </td>";
-    echo "</tr>";
-}
-?>
-<?php //DELETE CATEGORY QUERY
-if(isset($_GET['delete'])) {
-  $delete_cat_id = $_GET['delete'];
-  $query = "DELETE FROM categories ";
-  $query .= "WHERE cat_id = {$delete_cat_id} ";
-  $delete_query = mysqli_query($connection, $query);
-  header("Location: categories.php");
-}
-?>
+<!--FIND ALL CATEGORIES QUERY -->
+                          <?php findAllCategories();?>
+<!--DELETE CATEGORY QUERY -->
+                          <?php deleteCategory();?>
                         </tbody>
                       </table>
                     </div>
