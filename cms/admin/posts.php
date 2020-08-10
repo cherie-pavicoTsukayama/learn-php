14e8086f677657d7299677ff5e76f539bf98ff2a
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2020-08-10 10:20:59 -0700

    added a table and defined the table header

--- cms/admin/posts.php
+++ cms/admin/posts.php
@@ -11,7 +11,25 @@
             <!-- Page Heading -->
             <div class="row">
                 <div class="col-lg-12">
-                   
+                   <h1 class="page-header">
+                        Welcome to Admin
+                        <small>Author</small>
+                    </h1>
+                    <table>
+                      <thead>
+                        <tr>
+                          <th>Id</th>
+                          <th>Author</th>
+                          <th>Title</th>
+                          <th>Category</th>
+                          <th>Status</th>
+                          <th>Image</th>
+                          <th>Tags</th>
+                          <th>Comments</th>
+                          <th>Date</th>
+                        </tr>
+                      </thead>
+                    </table>
                 </div>
             </div>
             <!-- /.row -->
